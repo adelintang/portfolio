266f5626ff9f31eef2f3b91106e247fc169ceafd
adelintang <adelintang157@gmail.com>
2024-01-09 22:14:20 +0700

    add dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         'poppins': ['Poppins'],
       },
+      colors: {
+        dark: '#0F172A',
+      }
     },
   },
   plugins: [],

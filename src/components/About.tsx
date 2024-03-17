6a00905846671d239b39253b0a7f4b3d335fa1bd
adelintang <adelintang157@gmail.com>
2024-03-17 16:23:50 +0700

    perf(about): content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,11 +5,11 @@ export default function About() {
     <section id="about" className="pt-14 pb-10 bg-[#EDEEF0] dark:bg-slate-800">
       <div className="w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] mx-auto">
         <h2 className="text-lg font-semibold text-[#aaa]">#about</h2>
-        <p className="mt-4 dark:text-white">Saya adalah seorang mahasiswa dan sekarang tinggal di kota Surakarta, saya
-          tertarik untuk menekuni
-          bidang Web Development semenjak kuliah. Saya belajar banyak hal tentang Web Devolpment beserta
-          teknologi-teknologinya seperti HTML, CSS, Javascript. Dan sekarang saya mulai belajar Typescript sebagai
-          strongly type dari Javascript.</p>
+        <p className="mt-4 dark:text-white">
+          Saya adalah seorang mahasiswa Informatika dan sekarang tinggal di kota Surakarta, saya menekuni bidang Web Development semenjak kuliah.
+          Saya mempunyai beberapa keahlian seperti Javascript, React, NodeJS, Express.
+          Dan sekarang saya sedang memperdalam Typescript.
+        </p>
         <h2 className="text-lg font-semibold text-[#aaa] my-4">#skills</h2>
         <div className="flex gap-3 items-center">
           <img src="/icons/react.svg" alt="react-icon" className="w-8" />

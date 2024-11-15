374ae23f1c15389ebad2913dcb64482d4ea047c1
adelintang <adelintang157@gmail.com>
2024-11-15 23:49:56 +0700

    pref: profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,9 +4,9 @@ export default function Profile() {
       <div
         className="py-10 w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] mx-auto flex flex-col justify-center items-center lg:flex-row gap-2">
         <div>
-          <p className="text-[#2F74C8] text-lg font-semibold">Hai semuanya saya</p>
+          <p className="text-[#2F74C8] text-lg font-semibold">Hello Everyone</p>
           <p className="text-2xl font-semibold dark:text-white">Ade Lintang Permono</p>
-          <p className="dark:text-white">Fullstack Javascript | Informatics Engineering Student</p>
+          <p className="dark:text-white">Fullstack Javascript | Informatics Engineering Student College</p>
         </div>
         <img src="/img/adelintang.png" alt="adelintang" className="w-[400px]" loading="lazy" />
       </div>
